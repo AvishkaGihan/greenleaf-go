75ee4f4c4c282a8f581bcc4e4efcb323c42a81da
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2025-09-07 13:38:19 +0530

    feat: add initial components for Discover, Plan, Profile, Volunteer, Card, Header, Navigation, ProgressIndicator, and SearchBar

---
+++ mobile/app/discover/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const Discovers = () => {
+  return (
+    <View>
+      <Text>Discovers</Text>
+    </View>
+  );
+};
+
+export default Discovers;
