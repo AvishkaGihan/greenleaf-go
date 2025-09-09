c8dd9380994c0240140f9da4d671b9b879079249
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2025-09-09 10:36:18 +0530

    Revert "feat: Integrate react-native-dotenv for environment variable management; update .gitignore, babel and eslint configurations, and add dotenv dependency"
    This reverts commit cb36dd863d9ce7fe63ce15d59dad372557af2b30.

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -1,15 +1,10 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ["dist/*"],
-  },
-  {
-    rules: {
-      "import/no-unresolved": ["error", { ignore: ["@env"] }],
-    },
+    ignores: ['dist/*'],
   },
 ]);
